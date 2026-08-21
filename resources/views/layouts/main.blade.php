7644efc777a17f21ad7fa8981eee618afffc6932
Ashwin18 <ashwinkumar.c18@gmail.com>
2026-08-21 12:32:23 +0530

    Fix admin content alignment with compact sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,7 +21,7 @@
     @include('layouts.include')
     <link rel="stylesheet" href="{{ asset('assets/css/sidebar-responsive.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/admin-redesign-v23.css') }}?v=23.1">
-    <link rel="stylesheet" href="{{ asset('assets/css/sidebar-layout-v5.css') }}?v=5.1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/sidebar-layout-v5.css') }}?v=5.1.2">
     <link rel="stylesheet" href="{{ asset('assets/css/admin-workflow-review.css') }}?v=2.1.0">
     @yield('css')
     
